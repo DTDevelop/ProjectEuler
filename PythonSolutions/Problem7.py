b302310f15ee889c96df447e35b3a44e97647426
DTDevelop <46943548+DTDevelop@users.noreply.github.com>
2021-01-04 16:47:25 -0800

    Update Problem7.py
    spelling

--- PythonSolutions/Problem7.py
+++ PythonSolutions/Problem7.py
@@ -8,7 +8,7 @@ Problem 7
 QUESTIONS / SOLUTIONS
 """
 
-# by listing the first sixe prime numbers:
+# by listing the first six prime numbers:
 # 2, 3, 5, 7, 11, and 13, we can see the 6th prime is 13
 
 # what is the 10 0001st prime number?
