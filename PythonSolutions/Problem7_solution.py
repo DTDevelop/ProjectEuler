8eb2621529c9f0fc7236b10a9c8e0658b2252fa9
DTDevelop <46943548+DTDevelop@users.noreply.github.com>
2021-01-04 16:44:33 -0800

    Add files via upload

--- PythonSolutions/Problem7_solution.py
+++ PythonSolutions/Problem7_solution.py
@@ -10,12 +10,11 @@ def is_prime(num):
     given number, checks if prime number
     returns boolean
     """
+    if num < 0:
+        is_prime = False # if negeative number, return False
+        return is_prime
 
     is_prime = True
-
-    if num < 0:
-        is_prime = False
-        return is_prime
 
     divide_list = range(2,num) # divides against all values except for 1 and itself
     for check in divide_list:
@@ -27,6 +26,7 @@ def is_prime(num):
 
 # test case
 # known prime numbers from question: 2, 3, 5, 7, 11, 13
+# are the following prime numbers or not? [2, 3, 5, 7, 11, 13], [4, 6, 8, 9, 10, 12]
 # print(is_prime(2), is_prime(3), is_prime(5), is_prime(7), is_prime(11), is_prime(13))
 # print(is_prime(4), is_prime(6), is_prime(8), is_prime(9), is_prime(10), is_prime(12))
 
@@ -38,13 +38,33 @@ def is_prime(num):
 # True True True True True True
 # False False False False False False
 
-def prime_list(len):
+def prime_list(length, num):
     """
     given length
+    calculating from num and incrementing
     returns list of prime numbers of desired length
     """
 
+    prime_list = []
+    current_check = num
+
+    while len(prime_list) < length:
+        if is_prime(current_check):
+            prime_list.append(current_check)
+        current_check += 1
+
     return prime_list
+
+# test case
+# known prime numbers from question: 2, 3, 5, 7, 11, 13
+# return a list of prime numbers up to length 6, starting at 2
+print(prime_list(6,2))
+
+# expected output:
+# [2, 3, 5, 7, 11, 13]
+
+# actual output:
+# [2, 3, 5, 7, 11, 13]
 
 def return_prime_v1(ind):
     """
@@ -58,7 +78,7 @@ def return_prime_v1(ind):
 QUESTIONS / SOLUTIONS
 """
 
-# by listing the first sixe prime numbers:
+# by listing the first six prime numbers:
 # 2, 3, 5, 7, 11, and 13, we can see the 6th prime is 13
 
 print('\n')
