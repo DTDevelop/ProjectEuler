428fe65d4416c716d93515886c1f65f6bb34d795
DTDevelop <46943548+DTDevelop@users.noreply.github.com>
2021-01-05 20:41:49 -0800

    Add files via upload

---
+++ PythonSolutions/Problem10_abstraction.py
@@ -0,0 +1,50 @@
+"""
+Summation of primes
+Problem 10
+"""
+
+# HELPER FUNCTION
+
+def is_prime(num):
+    """
+    given number, checks if prime number
+    returns boolean
+    """
+
+    return is_prime
+
+# MAIN FUNCTION
+
+def prime_list(max):
+    """
+    given maximum
+    return list of primes below maximum
+    """
+
+    return prime_list
+
+"""
+QUESTIONS / SOLUTIONS
+"""
+
+# test case
+# the sum of the primes below 10 is 2 + 3 + 5 + 7 = 17
+
+print('\n')
+print('the primes below 10')
+my_primes = prime_list(10)
+print(my_primes)
+print('\n')
+print('sum of the primes below 10')
+print(sum(my_primes))
+
+# expected output:
+# [2, 3, 5, 7]
+# 17
+
+# find the sum of all the primes below two million
+
+print('\n')
+print('the sum of the primes below two million')
+my_primes = prime_list(2000000))
+print(sum(my_primes))
