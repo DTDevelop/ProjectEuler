2c2d4b5d24489af1de36df0c0f808ddf292031f0
DTDevelop <46943548+DTDevelop@users.noreply.github.com>
2021-01-08 14:46:37 -0800

    add problem/solution 14,16,17

---
+++ PythonSolutions/problems11-20/Problem16.py
@@ -0,0 +1,13 @@
+"""
+Power digit sum
+Problem 16
+"""
+
+
+"""
+QUESTIONS / SOLUTIONS
+"""
+
+# 2**15 = 322768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26
+
+# what is the sum of the digits of the number 2**1000
---
+++ PythonSolutions/problems11-20/Problem17.py
@@ -0,0 +1,21 @@
+"""
+Number letter counts
+Problem 17
+"""
+
+
+"""
+QUESTIONS / SOLUTIONS
+"""
+
+# if the numbers 1 to 5 are written out in words
+# eg. one, two, three, four, five
+# the letters used in total == 19
+
+# if all numbers from 1 to 1000 (one thousand) were written out in words
+# how many letters would be used?
+
+# spaces or hyphens do not count
+# eg. 342, three hundred and forty two contains 23 letters
+# eg. 115, one hundred and fifteen contains 20 letters
+# the 'and' is used in compliance with british usage
