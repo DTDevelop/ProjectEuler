71ff6a2fd8fe926aed42b79ed144944e095d1b8b
DTDevelop <46943548+DTDevelop@users.noreply.github.com>
2021-01-17 22:15:23 -0800

    upload problem 20 files

---
+++ PythonSolutions/problems11-20/Problem20.py
@@ -0,0 +1,16 @@
+"""
+Factorial digit sum
+Problem 20
+"""
+
+
+"""
+QUESTIONS / SOLUTIONS
+"""
+
+# n! means n * (n - 1) * ... * 3 * 2 * 1
+
+# eg. 10! == 10 * 9 * ... * 3 * 2 * 1 = 3628800
+# sum of the digits in the number 10! == 27
+
+#find the sum of the digits in the number 100!
---
+++ PythonSolutions/problems11-20/Problem20_abstraction.py
@@ -0,0 +1,43 @@
+"""
+Factorial digit sum
+Problem 20
+"""
+
+import math # using factorial function
+
+# or
+
+# HELPER FUNCTIONS
+
+def calc_factorial(number):
+    """
+    given number
+    returns factorial
+    """
+
+    return fact_num
+
+# MAIN FUNCTIONS
+
+def sum_of_digits(number):
+    """
+    given number
+    returns sum of digits
+    """
+
+    return sum
+
+
+
+"""
+QUESTIONS / SOLUTIONS
+"""
+
+# n! means n * (n - 1) * ... * 3 * 2 * 1
+
+# eg. 10! == 10 * 9 * ... * 3 * 2 * 1 = 3628800
+# sum of the digits in the number 10! == 27
+
+print(calc_factorial(10))
+
+#find the sum of the digits in the number 100!
